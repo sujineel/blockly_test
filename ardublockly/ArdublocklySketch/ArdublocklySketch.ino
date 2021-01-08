e8c803b636fa6c0dca20ea4c932aad2076409b10
sujineel <lsj990611@gmail.com>
2021-01-08 10:29:30 +0900

    블록클리 실행

---
+++ ardublockly/ArdublocklySketch/ArdublocklySketch.ino
@@ -0,0 +1,13 @@
+// Ardublockly generated sketch
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  analogWrite(3, 127);
+  analogWrite(5, 0);
+  analogWrite(6, 0);
+
+}
