2b3064a9f419302f159691da1e0e8e57a12eb3a0
sujineel <lsj990611@gmail.com>
2021-01-18 17:54:00 +0900

    온습도 센서 #define 선언 부분 코드 수정

--- ardublockly/blockly/generators/arduino/dht.js
+++ ardublockly/blockly/generators/arduino/dht.js
@@ -22,8 +22,8 @@ Blockly.Arduino['dht'] = function(block) {
 
   Blockly.Arduino.addInclude('dht', '#include "DHT.h"');
   //Blockly.Arduino.addDeclaration('servo_' + pinKey, 'Servo ' + servoName + ';');
-  Blockly.Arduino.addDeclaration('dht_'+ dhtpin, '#define DHTPIN '+ dhtpin +'\n' + '#define DHTTYPE DHT11' +'\n' +
-  'DHT dht'+dhtpin+'(DHTPIN, DHTTYPE)' +';')
+  Blockly.Arduino.addDeclaration('dht_'+ dhtpin, '#define DHTPIN'+ dhtpin+' '+dhtpin +'\n' + '#define DHTTYPE DHT11' +'\n' +
+  'DHT dht'+dhtpin+'(DHTPIN'+ dhtpin +' '+', DHTTYPE)' +';');
   //var setupCode = servoName + '.attach(' + pinKey + ');';
   var setupCode = 'dht'+ dhtpin +'.begin();';
   Blockly.Arduino.addSetup('dht_' + dhtpin, setupCode, true);
