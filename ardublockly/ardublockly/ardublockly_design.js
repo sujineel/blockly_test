8655041389286c96ac03a82e1ed4e80066ddafea
sujineel <lsj990611@gmail.com>
2021-02-16 17:08:10 +0900

    시리얼 버튼 onclick 함수 추가

--- ardublockly/ardublockly/ardublockly_design.js
+++ ardublockly/ardublockly/ardublockly_design.js
@@ -395,7 +395,29 @@ Ardublockly.resetIdeOutputContent = function(bodyEl) {
   ideOuputContent.innerHTML = '<span class="arduino_dialog_out">' +
       Ardublockly.getLocalStr('arduinoOpWaiting') + '</span>';
 };
+function openTextFile() {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
 
+    input.onchange = function (event) {
+
+        processFile(event.target.files[0]);
+
+    };
+
+    input.click();
+}
+
+function processFile(file) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        content_ide_output.innerText = reader.result;
+    };
+    reader.readAsText(file, /* optional */ "euc-kr");
+
+}
 /**
  * Initialises the sketch name input text JavaScript to dynamically adjust its
  * width to the width of its contents.
